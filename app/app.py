b6e27cee96d22670764337f03dfcb7dac30d14a3
Allendale Nato <natoallendale@gmail.com>
2016-10-28 13:38:44 +0800

    Fix select page

--- app/app.py
+++ app/app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return render_template('start.html')
+    return render_template('select.html')
 
 @app.route("/config", methods=['GET','POST'])
 def config():
     return render_template('config.html')
+
+@app.route("/copy", methods=['GET','POST'])
+def copy():
+    return render_template('loadCopy.html')
 
 if __name__ == "__main__":
     print('Starting...')
