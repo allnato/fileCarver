20c3d40b1df7c1516c1573d59e024935370c860b
Allendale Nato <natoallendale@gmail.com>
2016-10-28 17:07:04 +0800

    Add config UI

--- app/app.py
+++ app/app.py
@@ -4,10 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
+    return render_template('start.html')
+
+@app.route("/select", methods=['GET','POST'])
+def select():
     return render_template('select.html')
 
 @app.route("/config", methods=['GET','POST'])
 def config():
+    print('The configuration Page', file=sys.stderr)
     return render_template('config.html')
 
 @app.route("/copy", methods=['GET','POST'])
